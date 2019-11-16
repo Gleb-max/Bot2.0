951c645e8e166e7dc821983e8dbdd0aeb86898d7
Gleb-max <52007123+Gleb-max@users.noreply.github.com>
2019-11-16 17:15:51 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, request
+import telebot
+
+
+TOKEN = os.getenv('TG_API_TOKEN')
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Привет, %s!' % message.from_user.first_name)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+@server.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode('utf-8'))])
+    return '', 200
+
+
+if __name__ == '__main__':
+    bot.remove_webhook()
+    bot.set_webhook(url=os.getenv('WEBHOOK_URL') + TOKEN)
+
+    server.run(host="0.0.0.0", port=int(os.getenv('PORT', 8443)))
