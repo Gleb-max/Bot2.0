cec71f55e9fe88f9eb7d2b504dde913385cf7413
Gleb-max <52007123+Gleb-max@users.noreply.github.com>
2019-11-17 18:26:14 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import os
+import pydub
 from flask import Flask, request
 import telebot
+import speech_recognition as sr
 
 
+filename = 1
+ogg = "%d.ogg"
+wav = "%d.wav"
 INFO = "@{0}\nId: {1}\nFirst: {2}\nLast: {3}\nLang: {4}"
 GREETING = "Hi, {0}!"
 TOKEN = os.getenv('TG_API_TOKEN')
@@ -27,6 +32,22 @@ def echo_message(message):
     ))
 
 
+@bot.message_handler(content_types=["voice"])
+def save_voice(message):
+    global filename, ogg
+    voice_info = bot.get_file(message.voice.file_id)
+    voice_bytes = bot.download_file(voice_info.file_path)
+    with open(ogg % filename, 'wb') as file:
+        file.write(voice_bytes)
+    pydub.AudioSegment.from_ogg(ogg % filename).export(wav % filename, format='wav')
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(wav % filename) as source:
+        audio = recognizer.record(source)
+    text = recognizer.recognize_google(audio, language='ru-RU')
+    bot.send_message(message.chat.id, text)
+    filename += 1
+
+
 @server.route('/' + TOKEN, methods=['POST'])
 def get_message():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode('utf-8'))])
@@ -36,4 +57,5 @@ def get_message():
 if __name__ == '__main__':
     bot.remove_webhook()
     bot.set_webhook(url=os.getenv('WEBHOOK_URL') + TOKEN)
+    pydub.AudioSegment.converter = "ffmpeg-20191117-b741a84-win64-static/bin"
     server.run(host="0.0.0.0", port=int(os.getenv('PORT', 8443)))
